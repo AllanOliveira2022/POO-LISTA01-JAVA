1f06383ef9979bfe4ac8468204c3e6396222fc1a
AllanOliveira2022 <115299780+AllanOliveira2022@users.noreply.github.com>
2023-10-04 16:41:48 -0300

    Questão 02 da lista

---
+++ Q02.java
@@ -0,0 +1,11 @@
+public class Q02{
+    public static void main(String args []){
+        int soma = 0;
+        for(int i = 1; i <= 1000; i++){
+           soma += i + i; 
+        }
+        System.out.println("A soma dos números é: " +soma/2);
+
+
+    }
+}
